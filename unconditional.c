ed4885102122edbe3795a1fd721ab2487d4d5b85
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-10-02 19:11:41 +0530

    Create unconditional.c

---
+++ unconditional.c
@@ -0,0 +1,11 @@
+/*Write a program to print numbers from 1 to 100 without making use of conditional operators*/
+
+#include<stdio.h>
+int main()
+{
+int i=0;
+while(100 - i++)
+{
+    printf("%d\t",i);
+}
+}
