f4752b454c303dfe75c0c2a5ffb6915dce59d5f3
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-10-02 18:51:33 +0530

    swap_var.c

---
+++ swap_var.c
@@ -0,0 +1,33 @@
+/*How will you swap two variables? Write different approaches for the same*/
+
+#include<stdio.h>
+int main()
+{
+  //Using Extra Memory Space:
+int num1=20, num2=30, temp;
+temp = num1;
+num1 = num2;
+num2 = temp;
+  
+  //Using Arithmetic Operators:
+int num1=20, num2=30;
+num1=num1 + num2;
+num2=num1 - num2;
+num1=num1 - num2;
+  
+  //Using Bit-Wise Operators:
+int num1=20, num2=30;
+num1=num1 ^ num2;
+num2=num2 ^ num1;
+num1=num1 ^ num2;
+  
+  //Using One-liner Bit-wise Operators:
+int num1=20, num2=30;
+num1^=num2^=num1^=num2;  
+//The order of evaluation here is right to left.
+
+  //Using One-liner Arithmetic Operators:
+int num1=20, num2=30;
+num1 = (num1+num2)-(num2=num1);
+//Here the order of evaluation is from left to right.
+}
