26e2985201d5a5e82d6f2f842cf26451ea15bec9
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-12-16 14:14:31 +0530

    Update mul_by_9.c

--- mul_by_9.c
+++ mul_by_9.c
@@ -1,8 +1,13 @@
-/*Write an Embedded C program to multiply any number by 9 in the fastest manner*/
+/*Write an Embedded C program to multiply any number by 2,3,9 in the fastest manner*/
+
 #include<stdio.h>
-void main(){
+int main()
+{
     int num;
-    printf(“Enter number: ”);
-    scanf(“%d”,&num);
-    printf(“%d”, (num<<3)+num);
+    printf("Enter number:");
+    scanf("%d",&num);
+    printf("3 X %d = %d\n",num, ((num<<1)+(num)));
+    printf("2 x %d = %d\n", num, (num<<1));
+    printf("9 X %d = %d\n",num, (num<<3)+num);
+    return 0;
 }
